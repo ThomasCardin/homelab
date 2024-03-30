6b4b9269c046ba14d0c9e128a807f73f62eb2a64
ThomasCardin <tomcardin@outlook.com>
2024-03-30 18:20:05 -0400

    terraform application (wip)

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,24 @@ resource "cloudflare_access_application" "trilim-notes" {
   session_duration          = "24h"
   auto_redirect_to_identity = false
 }
+
+
+resource "cloudflare_access_policy" "test_policy" {
+  application_id = "cb029e245cfdd66dc8d2e570d5dd3322"
+  zone_id        = "0da42c8d2132a9ddaf714f9e7c920711"
+  name           = "staging policy"
+  precedence     = "1"
+  decision       = "allow"
+
+  include {
+    github {
+      identity_provider_id = "your_identity_provider_id"
+      name                = "your_github_name"
+      teams               = ["team1", "team2"]
+    }
+  }
+
+  require {
+    ip = [var.office_ip]
+  }
+}
