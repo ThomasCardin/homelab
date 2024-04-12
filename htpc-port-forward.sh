89ac673df5795f24b221c9e12500a42a0e256dca
ThomasCardin <tomcardin@outlook.com>
2024-04-12 10:00:30 -0400

    changed the script

--- htpc-port-forward.sh
+++ htpc-port-forward.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svc=("htpc-sonarr:8989" "htpc-deluge-openvpn:8112" "htpc-jackett:9117" "htpc-radarr:7878" "htpc-bazarr:6767" "plex-server-service:32400")
+svc=("htpc-sonarr:8989" "htpc-deluge-openvpn:8112" "htpc-jackett:9117" "htpc-radarr:7878" "htpc-bazarr:6767" "htpc-requestrr:4545" "plex-server-service:32400")
 
 for item in "${svc[@]}"
 do
@@ -11,7 +11,6 @@ do
     echo "kubectl port-forward svc/$service_name $port:$port"
     kubectl port-forward "svc/$service_name" "$port:$port" -n htpc &
     ps -ef | grep "kubectl port-forward svc/$service_name $port:$port" | grep -v grep
-    echo "Done"
     echo "===================="
 done
 
