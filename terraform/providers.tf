f041f70bbe1b4ebe87b99027eb0f7b19698b35e3
ThomasCardin <tomcardin@outlook.com>
2024-07-04 11:07:20 -0400

    required providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,13 @@ terraform {
       name = "homelab"
     }
   }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
 }
 
 provider "cloudflare" {
