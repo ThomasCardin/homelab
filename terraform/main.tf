21061780752a160641d54ba051dc04ace4628cde
ThomasCardin <tomcardin@outlook.com>
2024-07-04 10:40:23 -0400

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,31 @@
-resource "cloudflare_access_application" "trilim-notes" {
-  zone_id                   = var.cloudflare_access_zone_id
-  name                      = "Trilum notes"
-  domain                    = "trilium.ninebasetwo.net"
-  type                      = "self_hosted"
-  session_duration          = "24h"
-  auto_redirect_to_identity = false
+resource "cloudflare_record" "example" {
+  zone_id = var.cloudflare_zone_id
+  name    = "argocd"
+  value   = var.dns
+  type    = "CNAME"
+  ttl     = 3600
 }
 
+resource "cloudflare_record" "example" {
+  zone_id = var.cloudflare_zone_id
+  name    = "trilium"
+  value   = var.dns
+  type    = "CNAME"
+  ttl     = 3600
+}
 
-resource "cloudflare_access_policy" "test_policy" {
-  application_id = "cb029e245cfdd66dc8d2e570d5dd3322"
-  zone_id        = "0da42c8d2132a9ddaf714f9e7c920711"
-  name           = "staging policy"
-  precedence     = "1"
-  decision       = "allow"
+resource "cloudflare_record" "example" {
+  zone_id = var.cloudflare_zone_id
+  name    = "ollama"
+  value   = var.dns
+  type    = "CNAME"
+  ttl     = 3600
+}
 
-  include {
-    github {
-      identity_provider_id = "your_identity_provider_id"
-      name                = "your_github_name"
-      teams               = ["team1", "team2"]
-    }
-  }
-
-  require {
-    ip = [var.office_ip]
-  }
+resource "cloudflare_record" "example" {
+  zone_id = var.cloudflare_zone_id
+  name    = "ceph"
+  value   = var.dns
+  type    = "CNAME"
+  ttl     = 3600
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,19 @@
 variable "email" {
-    description = "The email address for Cloudflare"
-    type = string
+  description = "The email address for Cloudflare"
+  type = string
 }
 
 variable "cloudflare_api_key" {
-    description = "The API key for Cloudflare"
-    type = string
-}
-
-variable "cloudflare_access_account_id" {
-  description = "value of the account_id field in the Cloudflare Access configuration"
+  description = "The API key for Cloudflare"
   type = string
 }
 
-variable "cloudflare_access_zone_id" {
-  description = "value of the zone_id field in the Cloudflare Access configuration"
+variable "cloudflare_zone_id" {
+  description = "The zone ID for Cloudflare"
   type = string
 }
 
-variable "cloudflare_access_policy_name" {
-  description = "value of the name field in the Cloudflare Access configuration"
+variable "dns" {
+  description = "value for DNS record"
   type = string
 }
