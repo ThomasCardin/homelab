a2876ef1abe23efc0dde443483259321fa9c7418
ThomasCardin <tomcardin@outlook.com>
2024-07-04 11:05:40 -0400

    working-directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_record" "example" {
+resource "cloudflare_record" "cname_argocd" {
   zone_id = var.cloudflare_zone_id
   name    = "argocd"
   value   = var.dns
@@ -6,7 +6,7 @@ resource "cloudflare_record" "example" {
   ttl     = 3600
 }
 
-resource "cloudflare_record" "example" {
+resource "cloudflare_record" "cname_trilium" {
   zone_id = var.cloudflare_zone_id
   name    = "trilium"
   value   = var.dns
@@ -14,7 +14,7 @@ resource "cloudflare_record" "example" {
   ttl     = 3600
 }
 
-resource "cloudflare_record" "example" {
+resource "cloudflare_record" "cname_ollama" {
   zone_id = var.cloudflare_zone_id
   name    = "ollama"
   value   = var.dns
@@ -22,7 +22,7 @@ resource "cloudflare_record" "example" {
   ttl     = 3600
 }
 
-resource "cloudflare_record" "example" {
+resource "cloudflare_record" "cname_ceph" {
   zone_id = var.cloudflare_zone_id
   name    = "ceph"
   value   = var.dns
