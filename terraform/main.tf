435ddfa8eee56a26b2cfbc8691be0061b9c4b262
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-11-21 09:49:00 -0500

    converted records to tf module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,41 @@
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "argocd"
 }
 
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "trilium"
 }
 
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "ollama"
 }
 
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "ceph"
 }
 
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "grafana"
 }
 
 module "argocd-record" {
-  source = "../modules/records"
+  source = "./modules/records"
+  cloudflare_zone_id = var.cloudflare_zone_id
+
   name = "oauth2"
 }
