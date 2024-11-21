2c99c1d63c2bb7021d009f0c2e51ee6500998d42
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-11-21 10:10:42 -0500

    value to content

--- terraform/modules/dns_records/main.tf
+++ terraform/modules/dns_records/main.tf
@@ -1,9 +1,9 @@
 resource "cloudflare_dns_record" "record" {
-  for_each = var.names
+  for_each = { for idx, name in var.names : idx => name }
 
   zone_id = var.cloudflare_zone_id
-  value   = var.dns
-  name    = each.key
+  content = var.dns
+  name    = each.value
   type    = var.type
   ttl     = 1
   proxied = true
