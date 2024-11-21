baa7f6da0542143f8bc6ccc4a600e21deff78390
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-11-21 10:43:05 -0500

    5.0.0 reverted back to 4.0

--- terraform/modules/dns_records/main.tf
+++ terraform/modules/dns_records/main.tf
@@ -1,8 +1,8 @@
-resource "cloudflare_dns_record" "record" {
+resource "cloudflare_record" "record" {
   for_each = { for idx, name in var.names : idx => name }
 
   zone_id = var.cloudflare_zone_id
-  content = var.dns
+  value = var.dns
   name    = each.value
   type    = var.type
   ttl     = 1
