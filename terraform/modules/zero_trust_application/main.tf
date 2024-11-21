8c63ccde2b32e7b3cab3f04b04e16fec27d6689f
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-11-21 09:59:48 -0500

    changed record to for loop

---
+++ terraform/modules/zero_trust_application/main.tf
@@ -0,0 +1,12 @@
+resource "cloudflare_zero_trust_access_application" "app" {
+  zone_id                   = "0da42c8d2132a9ddaf714f9e7c920711"
+  name                      = "staging application"
+  domain                    = "staging.example.com"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  auto_redirect_to_identity = false
+  policies = [
+    cloudflare_zero_trust_access_policy.example_1.id,
+    cloudflare_zero_trust_access_policy.example_2.id
+  ]
+}
