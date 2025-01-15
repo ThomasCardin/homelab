89d25882d06d68eb678f0f061333adf406bce942
ThomasCardin <tomcardin@outlook.com>
2025-01-15 15:35:53 -0500

    atlantis

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "cname-records" {
   source             = "./modules/dns_records"
   cloudflare_zone_id = var.cloudflare_zone_id
 
-  names = ["argocd", "trilium", "ollama", "ceph", "grafana", "oauth2", "gotify", "actual"]
+  names = ["argocd", "trilium", "ollama", "ceph", "grafana", "oauth2", "gotify", "actual", "atlantis"]
   type  = "CNAME"
 }
 
