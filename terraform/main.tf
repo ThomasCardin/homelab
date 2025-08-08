ac2f3e8382031eed0543b9c09f2ac2391006a118
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2025-08-08 11:36:35 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "cname-records" {
   source             = "./modules/dns_records"
   cloudflare_zone_id = var.cloudflare_zone_id
 
-  names = ["argocd", "trilium", "ollama", "ceph", "grafana", "oauth2", "gotify", "actual", "atlantis", "alloy", "immich", "navidrome", "odoo"]
+  names = ["argocd", "trilium", "ollama", "ceph", "grafana", "oauth2", "gotify", "actual", "atlantis", "alloy", "immich", "navidrome", "odoo", "gobservability"]
   type  = "CNAME"
 }
 
@@ -10,5 +10,5 @@ module "zero-trust-app" {
   source             = "./modules/zero_trust_application"
   cloudflare_zone_id = var.cloudflare_zone_id
 
-  names = ["ollama", "argocd", "ceph", "grafana", "trilium", "actual", "alloy"]
+  names = ["ollama", "argocd", "ceph", "grafana", "trilium", "actual", "alloy", "gobservability"]
 }
